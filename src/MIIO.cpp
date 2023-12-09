ac54c04a0cf3443268855dc01ad67dece9b42934
uy_sun <hmy0119@gmail.com>
2023-12-09 13:39:21 +0800

    feat: 添加小米提供的 miio_define 和大概框架

--- src/MIIO.cpp
+++ src/MIIO.cpp
@@ -1,4 +1,5 @@
 #include "MIIO.h"
+#include "miio/miio_define.h"
 
 MIIO::MIIO(Stream& serial)
 {
@@ -7,6 +8,18 @@ MIIO::MIIO(Stream& serial)
 
 void MIIO::begin(const char* model, const char* blePid, const char* mcuVersion)
 {
+  _serial->setTimeout(_serialTimeoutMs);
+
+  {
+    int result = 0;
+    do {
+      result = MIIO_OK;
+      // TODO: 实现基本设置逻辑
+      // https://iot.mi.com/v2/new/doc/resources-and-services/personal-developer/embedded-dev#MCU%20程序开发
+
+      delay(_pollIntervalMs);
+    } while (result != MIIO_OK);
+  }
 }
 
 void MIIO::begin(String model, String blePid, String mcuVersion)
@@ -16,5 +29,21 @@ void MIIO::begin(String model, String blePid, String mcuVersion)
 
 void MIIO::loop()
 {
+  delay(_pollIntervalMs);
+  while (true)
+  {
+    // TODO: 实现命令处理逻辑
+  }
+
 }
 
+void MIIO::setSerialTimeout(unsigned long timeout)
+{
+  _serialTimeoutMs = timeout;
+  _serial->setTimeout(_serialTimeoutMs);
+}
+
+void MIIO::setPollInterval(unsigned long interval)
+{
+  _pollIntervalMs = interval;
+}
--- src/MIIO.h
+++ src/MIIO.h
@@ -13,8 +13,26 @@ public:
   void begin(String model, String blePid, String mcuVersion);
 
   void loop();
+
+
+  /* 串口超时
+
+  单位 ms
+  */
+  void setSerialTimeout(unsigned long timeout);
+
+  /* 轮询间隔
+
+  时间范围要求 100~200ms，推荐的循环周期为 200ms
+  https://iot.mi.com/v2/new/doc/embedded-dev/module-dev/function-dev/mcu-dev#命令规范
+  */
+  void setPollInterval(unsigned long interval);
+
+
 private:
   Stream* _serial;
+  unsigned long _serialTimeoutMs = 200;
+  unsigned long _pollIntervalMs = 200;
 };
 
 #endif
