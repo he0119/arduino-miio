2d2f37e7795b7fa7063900380b101e51b3b3c9d3
uy_sun <hmy0119@gmail.com>
2023-12-11 16:22:46 +0800

    style: 添加 pre-commit 并使用 clang-format 格式化代码
    配置参考自 koishijs/koishi-desktop

--- src/MIIO.h
+++ src/MIIO.h
@@ -21,18 +21,19 @@
 #ifndef _MIIO_H_
 #define _MIIO_H_
 
-#include "Arduino.h"
 #include <map>
 
- /* ==================== debug define ==================== */
+#include "Arduino.h"
+
+/* ==================== debug define ==================== */
 #ifndef NODEBUG_MIIO
 #ifdef DEBUG_ESP_PORT
-#define DEBUG_MIIO(...)               \
-    {                                       \
-        DEBUG_ESP_PORT.printf(__VA_ARGS__); \
-        DEBUG_ESP_PORT.println();           \
-        DEBUG_ESP_PORT.flush();             \
-    }
+#define DEBUG_MIIO(...)                                                        \
+  {                                                                            \
+    DEBUG_ESP_PORT.printf(__VA_ARGS__);                                        \
+    DEBUG_ESP_PORT.println();                                                  \
+    DEBUG_ESP_PORT.flush();                                                    \
+  }
 #endif
 #endif
 
@@ -43,20 +44,20 @@
 #endif
 #endif
 
- /* ==================== error define ==================== */
-#define MIIO_OK					(0)		/* There is no error		*/
-#define MIIO_ERROR				(-1)	/* A generic error happens	*/
-#define MIIO_ERROR_TIMEOUT		(-2)	/* Timed out				*/
-#define MIIO_ERROR_FULL			(-3)	/* The resource is full		*/
-#define MIIO_ERROR_EMPTY		(-4)	/* The resource is empty	*/
-#define MIIO_ERROR_NOMEM		(-5)	/* No memory				*/
-#define MIIO_ERROR_NOSYS		(-6)	/* No system				*/
-#define MIIO_ERROR_BUSY			(-7)	/* Busy						*/
-#define MIIO_ERROR_TRYOUT		(-8)	/* Try enough times			*/
-#define MIIO_ERROR_NOTFOUND		(-9)
-#define MIIO_ERROR_PARAM		(-10)
-#define MIIO_ERROR_SIZE			(-11)
-#define MIIO_ERROR_NOTREADY		(-12)
+/* ==================== error define ==================== */
+#define MIIO_OK (0)             /* There is no error		*/
+#define MIIO_ERROR (-1)         /* A generic error happens	*/
+#define MIIO_ERROR_TIMEOUT (-2) /* Timed out				*/
+#define MIIO_ERROR_FULL (-3)    /* The resource is full		*/
+#define MIIO_ERROR_EMPTY (-4)   /* The resource is empty	*/
+#define MIIO_ERROR_NOMEM (-5)   /* No memory				*/
+#define MIIO_ERROR_NOSYS (-6)   /* No system				*/
+#define MIIO_ERROR_BUSY (-7)    /* Busy    */
+#define MIIO_ERROR_TRYOUT (-8)  /* Try enough times			*/
+#define MIIO_ERROR_NOTFOUND (-9)
+#define MIIO_ERROR_PARAM (-10)
+#define MIIO_ERROR_SIZE (-11)
+#define MIIO_ERROR_NOTREADY (-12)
 
 enum uart_error_t {
   UART_OK = 0,
@@ -69,101 +70,89 @@ enum uart_error_t {
 };
 
 /* ==================== common string constants ==================== */
-#define SPACE_CHAR					' '
-#define SPACE_STRING				" "
-#define END_CHAR					'\r'
-#define ERROR_STRING				"error"
-#define OK_STRING				    "ok"
-#define SET_PRO_STRING				"set_properties"
-#define GET_PRO_STRING				"get_properties"
-#define NET_CHANGE_STRING			"MIIO_net_change"
-#define NONE_STRING					"none"
+#define SPACE_CHAR ' '
+#define SPACE_STRING " "
+#define END_CHAR '\r'
+#define ERROR_STRING "error"
+#define OK_STRING "ok"
+#define SET_PRO_STRING "set_properties"
+#define GET_PRO_STRING "get_properties"
+#define NET_CHANGE_STRING "MIIO_net_change"
+#define NONE_STRING "none"
 
 /* ==================== buffer size ==================== */
 /* buffer used to receive command string from wifi module */
-#define CMD_STR_MAX_LEN             (800) //max 800 byte
+#define CMD_STR_MAX_LEN (800) // max 800 byte
 /* max method name register with miio_cmd_method_register() function */
-#define CMD_METHOD_LEN_MAX          (64)
+#define CMD_METHOD_LEN_MAX (64)
 /* buffer used to response command execute result */
-#define RESULT_BUF_SIZE             CMD_STR_MAX_LEN
+#define RESULT_BUF_SIZE CMD_STR_MAX_LEN
 
-#define ACK_BUF_SIZE                CMD_STR_MAX_LEN
-#define DATA_STRING_MAX_LENGTH      (800)
+#define ACK_BUF_SIZE CMD_STR_MAX_LEN
+#define DATA_STRING_MAX_LENGTH (800)
 
 /* ==================== function define ==================== */
-typedef std::function<int(char* cmd, size_t length)> MethodCallback;
-
+typedef std::function<int(char *cmd, size_t length)> MethodCallback;
 
 /* ==================== property define ==================== */
-enum property_format_t
-{
+enum property_format_t {
   PROPERTY_FORMAT_UNDEFINED = 0,
   PROPERTY_FORMAT_BOOLEAN = 1,
   PROPERTY_FORMAT_STRING = 2,
   PROPERTY_FORMAT_NUMBER = 3,
 };
 
-struct data_boolean_t
-{
+struct data_boolean_t {
   bool value;
 };
 
-struct data_string_t
-{
+struct data_string_t {
   char value[DATA_STRING_MAX_LENGTH + 1];
   uint32_t length;
 };
 
-struct data_number_value_t
-{
+struct data_number_value_t {
   long integerValue;
   float floatValue;
 };
 
-enum data_number_type_t
-{
+enum data_number_type_t {
   DATA_NUMBER_INTEGER = 0,
   DATA_NUMBER_FLOAT = 1,
 };
 
-struct data_number_t
-{
-  data_number_type_t  type;
+struct data_number_t {
+  data_number_type_t type;
   data_number_value_t value;
 };
 
-union property_data_t
-{
-  data_boolean_t     boolean;
-  data_string_t      string;
-  data_number_t      number;
+union property_data_t {
+  data_boolean_t boolean;
+  data_string_t string;
+  data_number_t number;
 };
-struct property_value_t
-{
-  property_format_t   format;
-  property_data_t     data;
+struct property_value_t {
+  property_format_t format;
+  property_data_t data;
 };
 
-enum property_operation_type
-{
+enum property_operation_type {
   PROPERTY_OPERATION_GET = 0,
   PROPERTY_OPERATION_SET = 1,
 };
 
-struct property_operation_t
-{
-  uint32_t            siid;
-  uint32_t            piid;
-  int                 code;
-  property_value_t* value;
+struct property_operation_t {
+  uint32_t siid;
+  uint32_t piid;
+  int code;
+  property_value_t *value;
 };
 
-class MIIO
-{
+class MIIO {
 public:
-  MIIO(Stream& serial);
+  MIIO(Stream &serial);
 
-  void begin(const char* model, const char* blePid, const char* mcuVersion);
+  void begin(const char *model, const char *blePid, const char *mcuVersion);
 
   void begin(String model, String blePid, String mcuVersion);
 
@@ -186,33 +175,34 @@ public:
 
   int onCommand(String method, MethodCallback callback);
 
-  MethodCallback callbackFindByMethod(const char* method);
+  MethodCallback callbackFindByMethod(const char *method);
 
-  size_t sendStr(const char* str);
+  size_t sendStr(const char *str);
 
   size_t sendStr(String str);
 
-  size_t sendStrWaitAck(const char* str);
+  size_t sendStrWaitAck(const char *str);
 
   size_t sendStrWaitAck(String str);
 
-  size_t recvStr(char* buffer, size_t length);
+  size_t recvStr(char *buffer, size_t length);
 
-  int sendPropertyChanged(uint32_t siid, uint32_t piid, property_value_t* newValue);
+  int sendPropertyChanged(
+      uint32_t siid, uint32_t piid, property_value_t *newValue);
 
-  int executePropertyChanged(property_operation_t& opt);
+  int executePropertyChanged(property_operation_t &opt);
 
-  int uartComamndDecoder(char* pbuf, size_t buf_sz, char* method, size_t* methodLen);
-
+  int uartComamndDecoder(
+      char *pbuf, size_t buf_sz, char *method, size_t *methodLen);
 
 private:
-  Stream* _serial;
+  Stream *_serial;
   unsigned long _pollIntervalMs = 200;
   unsigned long _lastPoll = 0;
   unsigned int _receiveRetry = 25;
 
-  char _pbuf[CMD_STR_MAX_LEN] = { 0 };
-  char _method[CMD_METHOD_LEN_MAX] = { 0 };
+  char _pbuf[CMD_STR_MAX_LEN] = {0};
+  char _method[CMD_METHOD_LEN_MAX] = {0};
   std::map<String, MethodCallback> _commands;
 };
 
